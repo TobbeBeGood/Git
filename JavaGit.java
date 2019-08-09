162bedb846f92f4a2e232969f071a5af441a15c0
Torben Schrötke <torben.schroetke@gmail.com>
2019-08-09 11:58:18 +0200

    neues Projekt, Git mit Java, Hauptklasse erstellt

---
+++ JavaGit.java
@@ -0,0 +1,16 @@
+public class JavaGit {
+
+
+	public static void main (String[] args){
+
+		//Methode zum Ausführen einer Benutzer-bestimmten Funktion
+		System.out.println("this program has a couple of functions to train the use of Git with sourcecode");
+
+	}
+
+
+
+
+
+
+}
