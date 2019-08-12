f850eed9428566822174e5730eaac6674dc7ad77
Torben Schrötke <torben.schroetke@gmail.com>
2019-08-12 16:33:11 +0200

    kleione änderung

--- JavaGit.java
+++ JavaGit.java
@@ -6,7 +6,7 @@ public class JavaGit {
 	static Scanner notiz =new Scanner(System.in);
 	static Scanner seite =new Scanner(System.in);
 
-	static int befehl;																						//Variable für Benutzereingabe
+	static int befehl=5;																						//Variable für Benutzereingabe
 
 	public static void main (String[] args){
 
